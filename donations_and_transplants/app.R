4dc5e2594a02d600cb61066da54ff10db0ee599c
Shafi Rubbani <shafirubbani@gmail.com>
2019-04-23 17:25:38 -0400

    Chose a layout

--- donations_and_transplants/app.R
+++ donations_and_transplants/app.R
@@ -12,45 +12,89 @@ library(tidyverse)
 source("data_helper.R")
 
 # Define UI for application that draws a histogram
-ui <- fluidPage(
-  
+ui <- 
+
+# fluidPage(
+#   titlePanel("Organ Donations and Transplants"),
+#   helpText("A Gov 1005 Final Project by Shafi Rubbani, Spring 2019"),
+#   sidebarLayout(
+#     sidebarPanel(
+#       helpText("In this project, I explore the magic of Taco Tuesday, ay! Taquito Thursday, oh!")
+#     ),
+#     mainPanel(
+#       tabsetPanel(type = "tabs",
+#         tabPanel("Plot",
+#                  sidebarLayout(position = "right",
+#                    sidebarPanel(
+#                      selectInput(inputId = "country",
+#                                  label = "Country",
+#                                  choices = countries,
+#                                  selected = "AU",
+#                                  multiple = FALSE),
+#                      selectInput(inputId = "organ",
+#                                  label = "Organ",
+#                                  choices = organs,
+#                                  selected = "kidney",
+#                                  multiple = FALSE),
+#                      selectInput(inputId = "measure",
+#                                  label = "Measure",
+#                                  choices = c(
+#                                    "Number" = "num",
+#                                    "Per Million People" = "pmp"
+#                                  ),
+#                                  selected = "num",
+#                                  multiple = FALSE)
+#                    ),
+#                    mainPanel()
+#                  )
+#                  ),
+#         tabPanel("Taco"),
+#         tabPanel("Tuesday")
+#       )
+#     )
+#   )
+# )
+
+# tabPanel("Plot"),
+# tabPanel("Taco"),
+# tabPanel("Tuesday")
+
+fluidPage(
   # Application title
   titlePanel("Organ Donations and Transplants"),
   helpText("A Gov 1005 Final Project by Shafi Rubbani, Spring 2019"),
   
-  # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
-      selectInput(inputId = "country",
-                  label = "Country",
-                  choices = countries,
-                  selected = "AU",
-                  multiple = FALSE),
-      selectInput(inputId = "organ",
-                  label = "Organ",
-                  choices = organs,
-                  selected = "kidney",
-                  multiple = FALSE),
-      selectInput(inputId = "measure",
-                  label = "Measure",
-                  choices = c(
-                    "Number" = "num",
-                    "Per Million People" = "pmp"
-                  ),
-                  selected = "num",
-                  multiple = FALSE)
-    ),
-    
-    # Show a plot of the generated distribution
-    mainPanel(
-      tabsetPanel(type = "tabs",
-                  tabPanel("Plot", 
-                           plotOutput("donationsPlot")),
-                  tabPanel("Summary"),
-                  tabPanel("About")
-      )
+  # Show a plot of the generated distribution
+  tabsetPanel(type = "tabs",
+                tabPanel("Plot",
+                         # Sidebar with a slider input for number of bins
+                         sidebarLayout(
+                           mainPanel(plotOutput("donationsPlot")),
+                           sidebarPanel(
+                             selectInput(inputId = "country",
+                                         label = "Country",
+                                         choices = countries,
+                                         selected = "AU",
+                                         multiple = FALSE),
+                             selectInput(inputId = "organ",
+                                         label = "Organ",
+                                         choices = organs,
+                                         selected = "kidney",
+                                         multiple = FALSE),
+                             selectInput(inputId = "measure",
+                                         label = "Measure",
+                                         choices = c(
+                                           "Number" = "num",
+                                           "Per Million People" = "pmp"
+                                         ),
+                                         selected = "num",
+                                         multiple = FALSE)
+                           )
+                         ),
+                tabPanel("Summary"),
+                tabPanel("About")
+                )
     )
-  )
 )
 
 # Define server logic required to draw a histogram
